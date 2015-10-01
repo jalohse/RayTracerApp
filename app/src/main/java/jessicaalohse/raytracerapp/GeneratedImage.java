3b8557d71a9db3bf927ddf74b4460d53ffc736c2
Jessica Lohse <jessicaalohse@gmail.com>
2015-10-01 18:25:15 -0400

    Removing menus

--- app/src/main/java/jessicaalohse/raytracerapp/GeneratedImage.java
+++ app/src/main/java/jessicaalohse/raytracerapp/GeneratedImage.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -21,27 +19,5 @@ public class GeneratedImage extends Activity {
         Intent intent = getIntent();
         Bitmap bitmap = BitmapFactory.decodeFile(intent.getStringExtra("PATH"));
         image.setImageBitmap(bitmap);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_generated_iamge, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
